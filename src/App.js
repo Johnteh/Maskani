3dd4dc74faddf72d33610d4a28376aa151e76446
Johnteh <miirijohn08@gmail.com>
2023-07-17 20:32:37 +0000

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Profile from"./pages/Profile";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className='text-2xl bg-red'>hello world</h1>
-    </div>
+   <>
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+
+      </Routes>
+    </Router>
+   
+   </>
   );
 }
 
