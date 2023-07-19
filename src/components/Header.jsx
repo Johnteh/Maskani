1ed9f98965999c3606b252606c9a646db556f52f
Johnteh <miirijohn08@gmail.com>
2023-07-19 20:37:30 +0000

    authentication frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold
                      text-gray-400 border-b-[3px] border-b-transparent
-                     ${pathMathRoute("/") && "text-black border-b-red-500"}`}
+                     ${pathMathRoute("/") && 'text-black border-b-red-500'}`}
                      onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold
                      text-gray-400 border-b-[3px] border-b-transparent
