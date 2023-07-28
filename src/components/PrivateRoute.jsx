c6fbb4e612e4f8889e786bf49e91a4acadecf64d
Johnteh <miirijohn08@gmail.com>
2023-07-28 13:40:48 +0000

    added spinner component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,12 @@
 
 import { Outlet, Navigate } from 'react-router';
 import { useAuthStatus } from '../hooks/useAuthStatus';
-
+import Spinner from "./Spinner";
 export default  function PrivateRoute() {
     const {loggedIn, checkingStatus} = useAuthStatus();
 
     if(checkingStatus) {
-        return <h3>...loading</h3>
+        return <Spinner />;
     }
     return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
