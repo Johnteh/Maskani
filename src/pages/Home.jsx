6feff5343ab8ad1d8daa22a7d61afeb34dcc2620
Johnteh <miirijohn08@gmail.com>
2023-08-15 00:01:31 +0000

    added recent offers rent and sale to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,187 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from '../components/Slider';
+import { collection, limit, getDocs, orderBy, query, where, } from "firebase/firestore";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
+
+  //offers
+  const[offerListings, setOfferListings] = useState(null);
+
+  useEffect(()=>{
+    async function fetchListings(){
+      try {
+        //get reference
+        const listingsRef = collection(db, "listings")
+        //create the query
+        const q = query(listingsRef, where("offer", "==", true), orderBy("timestamp", "desc"), limit(4));
+
+        //execute the query
+        const querySnap = await getDocs(q)
+
+        const listings =[];
+
+        //loop through the query to add to the list
+        querySnap.forEach((doc)=>{
+          return listings.push({
+            //add it as an object
+            id:doc.id,
+            data:doc.data(),
+          });
+        });
+
+        setOfferListings(listings)
+        
+
+        
+      } catch (error) {
+
+        console.log(error);
+        
+      }
+
+    }
+    fetchListings();
+
+  }, []);
+  //rent
+  const[rentListings, setRentListings] = useState(null);
+
+  useEffect(()=>{
+    async function fetchListings(){
+      try {
+        //get reference
+        const listingsRef = collection(db, "listings")
+        //create the query
+        const q = query(listingsRef, where("type", "==", "rent"), orderBy("timestamp", "desc"), limit(4));
+
+        //execute the query
+        const querySnap = await getDocs(q)
+
+        const listings =[];
+
+        //loop through the query to add to the list
+        querySnap.forEach((doc)=>{
+          return listings.push({
+            //add it as an object
+            id:doc.id,
+            data:doc.data(),
+          });
+        });
+
+        setRentListings(listings)
+        
+
+        
+      } catch (error) {
+
+        console.log(error);
+        
+      }
+
+    }
+    fetchListings();
+
+  }, []);
+  
+  //sale
+  const[saleListings, setSaleListings] = useState(null);
+
+  useEffect(()=>{
+    async function fetchListings(){
+      try {
+        //get reference
+        const listingsRef = collection(db, "listings")
+        //create the query
+        const q = query(listingsRef, where("type", "==", "sale"), orderBy("timestamp", "desc"), limit(4));
+
+        //execute the query
+        const querySnap = await getDocs(q)
+
+        const listings =[];
+
+        //loop through the query to add to the list
+        querySnap.forEach((doc)=>{
+          return listings.push({
+            //add it as an object
+            id:doc.id,
+            data:doc.data(),
+          });
+        });
+
+        setSaleListings(listings)
+        
+
+        
+      } catch (error) {
+
+        console.log(error);
+        
+      }
+
+    }
+    fetchListings();
+
+  }, []);
   
   return (
     <>
     <Slider />
+    <div className="max-w-6xl mx-auto pt-4 space-y-6">
+      {offerListings && offerListings.length > 0 && (
+        <div className="m-2 mb-6">
+          <h2 className="px-3 text-2xl mt-6 font-semibold"> Recent Offers</h2>
+          <Link to="/offers">
+            <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150
+            ease-in-out">view more offers</p>
+          </Link>
+          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {offerListings.map((listing)=>(
+              <ListingItem 
+              key={listing.id} 
+              listing={listing.data}
+              id={listing.id}/>
+            ))}
+          </ul>
+        </div>
+      )}
+      {rentListings && rentListings.length > 0 && (
+        <div className="m-2 mb-6">
+          <h2 className="px-3 text-2xl mt-6 font-semibold"> Places For Rent</h2>
+          <Link to="/category/rent">
+            <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150
+            ease-in-out">view more places for rent</p>
+          </Link>
+          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {rentListings.map((listing)=>(
+              <ListingItem 
+              key={listing.id} 
+              listing={listing.data}
+              id={listing.id}/>
+            ))}
+          </ul>
+        </div>
+      )}
+      {saleListings && saleListings.length > 0 && (
+        <div className="m-2 mb-6">
+          <h2 className="px-3 text-2xl mt-6 font-semibold"> Places For sale</h2>
+          <Link to="/category/sale">
+            <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150
+            ease-in-out">view more places for sale</p>
+          </Link>
+          <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {saleListings.map((listing)=>(
+              <ListingItem 
+              key={listing.id} 
+              listing={listing.data}
+              id={listing.id}/>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
     </>
   )
 }
